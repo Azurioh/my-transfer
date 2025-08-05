c916d30c389c5218307a6969b6b49af6594ebd41
Alan CUNIN <alan.cunin@epitech.eu>
2025-08-05 13:33:03 +0200

    feat(backend): add new environment variables to the .env_example file

--- apps/backend/src/types/env.d.ts
+++ apps/backend/src/types/env.d.ts
@@ -1,7 +1,12 @@
-namespace NodeJS {
+declare namespace NodeJS {
   interface ProcessEnv {
-    PORT: string;
-    API_BASE_URL: string;
     NODE_ENV: string;
+    API_BASE_URL: string;
+    PORT: string;
+    MONGO_URI: string;
+    MONGO_DATABASE: string;
+    PASSWORD_SECRET: string;
+    JWT_SECRET: string;
+    JWT_REFRESH_SECRET: string;
   }
 }
