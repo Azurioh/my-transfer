1832947226cf859c3d4934b44666e77a2a425fc5
Alan CUNIN <alan.cunin@epitech.eu>
2025-08-05 22:20:50 +0200

    feat(backend): separate the generate tokens functions

--- apps/backend/src/routes/auth/utils/generate-tokens.ts
+++ apps/backend/src/routes/auth/utils/generate-tokens.ts
@@ -15,16 +15,37 @@ export const generateTokens = (
   user: WithId<User>,
   rememberMe: boolean,
 ): { accessToken: string; refreshToken: string } => {
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user, rememberMe);
+
+  return { accessToken, refreshToken };
+};
+
+/**
+ * @description Generates an access token from a refresh token
+ *
+ * @param refreshToken - The refresh token to generate an access token from
+ * @returns The access token
+ */
+export const generateAccessToken = (user: WithId<User>) => {
   const accessTokenPayload = accessTokenSchema.parse({ id: user._id.toString(), name: user.name, email: user.email });
+  const accessToken = jwt.sign(accessTokenPayload, environment.JWT_SECRET, { expiresIn: '15m' });
+
+  return accessToken;
+};
+
+/**
+ * @description Generates a refresh token for a user
+ *
+ * @param user - The user to generate a refresh token for
+ * @param rememberMe - Whether to remember the user
+ * @returns The refresh token
+ */
+export const generateRefreshToken = (user: WithId<User>, rememberMe: boolean) => {
   const refreshTokenPayload = refreshTokenSchema.parse({ id: user._id.toString() });
-
-  const jwtSecret = environment.JWT_SECRET;
-  const jwtRefreshSecret = environment.JWT_REFRESH_SECRET;
-
-  const accessToken = jwt.sign(accessTokenPayload, jwtSecret, { expiresIn: '15m' });
-  const refreshToken = jwt.sign(refreshTokenPayload, jwtRefreshSecret, {
+  const refreshToken = jwt.sign(refreshTokenPayload, environment.JWT_REFRESH_SECRET, {
     expiresIn: rememberMe ? '30d' : '1d',
   });
 
-  return { accessToken, refreshToken };
+  return refreshToken;
 };
