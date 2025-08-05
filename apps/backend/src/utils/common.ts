04746af1931e1325b805c1c711e68d60bcb94407
Alan CUNIN <alan.cunin@epitech.eu>
2025-08-05 13:22:25 +0200

    feat(backend): create a common utils file

---
+++ apps/backend/src/utils/common.ts
@@ -0,0 +1,66 @@
+import type {} from 'fastify';
+import type { z } from 'zod';
+
+/**
+ * @type Maybe
+ *
+ * @description A type that represents a value that can be null or undefined.
+ */
+export type Maybe<T> = T | null | undefined;
+
+/**
+ * @description Excludes properties from an object.
+ *
+ * @param obj - The object to exclude properties from.
+ * @param properties - The properties to exclude.
+ *
+ * @returns The object with the properties excluded.
+ *
+ * @example
+ * const obj = { a: 1, b: 2, c: 3 };
+ * const properties = ['b', 'c'];
+ * const newObj = excludeProperties(obj, properties);
+ * // newObj = { a: 1 }
+ */
+export const excludeProperties = <T extends Record<string, unknown>, K extends keyof T>(obj: T, properties: K[]): T => {
+  return Object.fromEntries(Object.entries(obj).filter(([key]) => !properties.includes(key as K))) as T;
+};
+
+/**
+ * @description Excludes properties from an object recursively.
+ *
+ * @param obj - The object to exclude properties from.
+ * @param properties - The properties to exclude.
+ *
+ * @returns The object with the properties excluded.
+ */
+export const excludePropertiesRecursively = <T extends Record<string, unknown>, K extends keyof T>(
+  obj: T,
+  properties: K[],
+): T => {
+  return Object.fromEntries(
+    Object.entries(obj)
+      .filter(([key]) => !properties.includes(key as K))
+      .map(([key, value]) => [
+        key,
+        typeof value === 'object' && value !== null
+          ? excludePropertiesRecursively(value as Record<string, unknown>, properties as string[])
+          : value,
+      ]),
+  ) as T;
+};
+
+/**
+ * @description Converts a zod JSON schema to an AJV schema.
+ *
+ * @param schema - The zod JSON schema to convert.
+ *
+ * @returns The AJV schema.
+ */
+export const zodJsonSchemaToAjvSchema = (schema: z.core.JSONSchema.JSONSchema) => ({
+  ...excludeProperties(schema, ['$schema']),
+  ...(schema.properties
+    ? { properties: excludePropertiesRecursively(schema.properties, ['example']) }
+    : { properties: {} }),
+  required: Object.entries(schema.required ?? {}).map(([_, value]) => value),
+});
