f4f1914ad1ef1a87f0f8882b2565375c3614b3c2
Alan CUNIN <alan.cunin@epitech.eu>
2025-08-05 13:31:24 +0200

    feat(backend): create utils function for auth service to generate tokens

---
+++ apps/backend/src/routes/auth/utils/generate-tokens.ts
@@ -0,0 +1,26 @@
+import { accessTokenSchema, refreshTokenSchema } from '@schemas/token';
+import type { User } from '@schemas/user';
+import jwt from 'jsonwebtoken';
+import type { WithId } from 'mongodb';
+
+/**
+ * @description Generates access and refresh tokens for a user
+ *
+ * @param user - The user to generate tokens for
+ * @param rememberMe - Whether to remember the user
+ * @returns The access and refresh tokens
+ */
+export const generateTokens = (
+  user: WithId<User>,
+  rememberMe: boolean,
+): { accessToken: string; refreshToken: string } => {
+  const accessTokenPayload = accessTokenSchema.parse({ id: user._id.toString(), name: user.name, email: user.email });
+  const refreshTokenPayload = refreshTokenSchema.parse({ id: user._id.toString() });
+
+  const accessToken = jwt.sign(accessTokenPayload, process.env.JWT_SECRET || '', { expiresIn: '15m' });
+  const refreshToken = jwt.sign(refreshTokenPayload, process.env.JWT_REFRESH_SECRET || '', {
+    expiresIn: rememberMe ? '30d' : '1d',
+  });
+
+  return { accessToken, refreshToken };
+};
